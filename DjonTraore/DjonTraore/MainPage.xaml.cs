a6f85325d811d466e222f47da22cbb4be65f06b2
mustaphakd <mustaphakd@gmail.com>
2016-10-23 18:21:25 +0000

    Add project files.

---
+++ DjonTraore/DjonTraore/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using StandardPCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DjonTraore
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public string MainText { get; private set; } = "Hello Xamarin";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var tapGesture = new TapGestureRecognizer();
+            tapGesture.Command = new Command(() => {
+
+                var muezzin = new Muezzin();
+                DisplayAlert("Alert", " It is currently: " + muezzin.GetCurrentTime(), "Ok");
+            });
+            bxvwColor.GestureRecognizers.Add(tapGesture);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            var random = new Random();
+
+            var muezzin = new Muezzin();
+            var counter = 0;
+
+            Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+            {
+                MainText = muezzin.Callout() + "," + counter++;
+                var val = random.Next(0, 1);
+                bxvwColor.Color = val == 0 ? Color.Accent : Color.Fuchsia;
+                return true;
+            });
+        }
+    }
+}
---
+++ StandardPCL/Muezzin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardPCL
+{
+    public class Muezzin
+    {
+        public string Callout() { return "Hello";  }
+
+        public string GetCurrentTime()
+        {
+            return DateTimeOffset.Now.TimeOfDay.ToString();
+        }
+    }
+
+
+}
